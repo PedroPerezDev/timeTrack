7de2f2cd27ea721e87091efac50a8a8c44471b01
Pedro <perez.pedro.dev@gmail.com>
2026-04-25 19:57:01 +0200

    creado el index de trabajador con función minima

--- trabajador/index.php
+++ trabajador/index.php
@@ -1,12 +1,15 @@
 <?php
 /*
  * Panel principal del trabajador
+ * Muestra el horario del día y el botón de fichar
  */
 session_start();
 include "../includes/header.php";
 ?>
 
-<h2>Mi jornada de hoy</h2>
-<p>Bienvenido, <?php echo $_SESSION['user']; ?></p>
+<main>
+    <h2>Mi jornada de hoy</h2>
+    <p>Bienvenido, <?php echo $_SESSION['user']; ?></p>
+</main>
 
 <?php include "../includes/footer.php"; ?>
