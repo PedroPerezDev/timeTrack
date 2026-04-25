d4ee159da468c9a439ea40e1032982de1ffdc6ed
Pedro <perez.pedro.dev@gmail.com>
2026-04-25 20:27:36 +0200

    creado el reloj en la pantalla de trabajador + funcion js

--- trabajador/index.php
+++ trabajador/index.php
@@ -10,6 +10,16 @@ include "../includes/header.php";
 <main>
     <h2>Mi jornada de hoy</h2>
     <p>Bienvenido, <?php echo $_SESSION['user']; ?></p>
+
+    <!-- Reloj en tiempo real -->
+    <!-- El JS busca los ids 'reloj' y 'fecha' y los actualiza cada segundo -->
+    <div class="reloj-wrapper">
+        <span id="reloj"></span>
+        <span id="fecha"></span>
+        <button type="button" id="btn-fichar">FICHAR</button>
+    </div>
+
 </main>
 
+<script src="/timetrack/js/main.js"></script>
 <?php include "../includes/footer.php"; ?>
